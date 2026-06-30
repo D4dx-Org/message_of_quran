238eaf10af08b3282fc39472f2c6d275df1b2866
Nihalmullungal <nihalmullungal@gmail.com>
2026-06-30 12:35:08 +0530

    version chaged

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   AppConstants._();
   static const double appBarIconWidth = 24;
   static const double appBarIconHeight = 24;
-  static const String appVersion = "1.0.10";
+  static const String appVersion = "1.0.11";
 
   /// Display name used in share text and branding.
   static const String appName = 'Quran Asad Malayalam';
